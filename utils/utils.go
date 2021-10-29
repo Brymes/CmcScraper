5a44590ef3f296ee4f4e5a7bfa069e4c6f7d70b6
Timothy Odebunmi <dev.brymes@gmail.com>
2021-10-29 01:12:06 +0100

    Functions for timed chrome contexts & check errors

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,33 @@
 package utils
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/chromedp/chromedp"
+	"log"
+	"time"
+)
+
+var devtoolsWsURL = flag.String("devtoolsWSurl", "http://127.0.0.1:9222/", "DevTools Websocket URL")
+var allocatorContext, ctxt context.Context
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GenerateChromeContext(age int64) context.Context {
+	allocatorContext, _ =
+		chromedp.NewRemoteAllocator(context.Background(),
+			*devtoolsWsURL)
+
+	ctxt, _ = chromedp.NewContext(allocatorContext)
+
+	// Parse context age to Duration as Seconds
+	dur, _ := time.ParseDuration(fmt.Sprintf("%vs", age))
+	ctxt, _ = context.WithTimeout(ctxt, time.Duration(dur))
+
+	return ctxt
+}
