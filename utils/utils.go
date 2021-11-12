2a41c513bdec850e49ebffc69c0dc4a49f40e88c
Timothy Odebunmi <dev.brymes@gmail.com>
2021-11-12 22:36:32 +0100

    Util Function to load Config from file

--- utils/utils.go
+++ utils/utils.go
@@ -2,10 +2,12 @@ package utils
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/chromedp/chromedp"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -40,5 +42,34 @@ func ParseStringToInt(quantity string) (int, error) {
 	chars := strings.Split(temp[1], ",")
 
 	return strconv.Atoi(strings.Join(chars, ""))
+}
+
+func ReadConfigFromJSON(file string) Config {
+	var data Config
+
+	//FIXME
+	filePath := fmt.Sprintf("%v", file)
+
+	dataFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	defer func(dataFile *os.File) {
+		err := dataFile.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}(dataFile)
+
+	jsonParser := json.NewDecoder(dataFile)
+	err = jsonParser.Decode(&data)
+
+	if err != nil {
+		panic("Something Wrong with Config file")
+	}
+	fmt.Println(data)
+
+	return data
 }
 
