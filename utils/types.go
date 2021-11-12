78a3ebadb338f2d925099a73b416283d0b727642
Timothy Odebunmi <dev.brymes@gmail.com>
2021-11-12 21:13:04 +0100

    added  struct to hold raw scraped data for parsing

--- utils/types.go
+++ utils/types.go
@@ -1,17 +1,34 @@
 package utils
 
-import "time"
-
-type Coin struct {
+type ParsedCoin struct {
 	Name        string
 	Price       int
 	AllTimeHigh int
 	AllTimeLow  int
-	Volume      int64
-	MarketCap   int64
+
+	MarketCap   int
+	Supply      int
+
+	// Volume is 24 hours volume in USD, It could be changed to 7days or 30 days
+	//and can also be measured in the Currency Not only in USD
+	Volume      int
 
 	// Created and Updated if we were to use some sort of SQL structure
 	// Gorm would be a good pick for storing this to SQL
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	/*CreatedAt time.Time
+	UpdatedAt time.Time*/
+}
+
+type RawCoinStruct struct {
+	Name        string
+	Price       string
+	AllTimeHigh string
+	AllTimeLow  string
+
+	MarketCap   string
+	Supply      string
+
+	// Volume is 24 hours volume in USD, It could be changed to 7days or 30 days
+	//and can also be measured in the Currency Not only in USD
+	Volume      string
 }
