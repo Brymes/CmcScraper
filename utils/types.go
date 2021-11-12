8a5a341145cf3da0e2a12a63a004835747631521
Timothy Odebunmi <dev.brymes@gmail.com>
2021-11-12 22:33:55 +0100

    Type to laoad from config.json

--- utils/types.go
+++ utils/types.go
@@ -6,12 +6,12 @@ type ParsedCoin struct {
 	AllTimeHigh int
 	AllTimeLow  int
 
-	MarketCap   int
-	Supply      int
+	MarketCap int
+	Supply    int
 
 	// Volume is 24 hours volume in USD, It could be changed to 7days or 30 days
 	//and can also be measured in the Currency Not only in USD
-	Volume      int
+	Volume int
 
 	// Created and Updated if we were to use some sort of SQL structure
 	// Gorm would be a good pick for storing this to SQL
@@ -25,10 +25,17 @@ type RawCoinStruct struct {
 	AllTimeHigh string
 	AllTimeLow  string
 
-	MarketCap   string
-	Supply      string
+	MarketCap string
+	Supply    string
 
 	// Volume is 24 hours volume in USD, It could be changed to 7days or 30 days
 	//and can also be measured in the Currency Not only in USD
-	Volume      string
+	Volume string
+}
+
+type Config struct {
+	MinMarketCap int `json:"minMarketCap"`
+	MaxMarketCap int `json:"maxMarketCap"`
+	MinSupply    int `json:"minSupply"`
+	MaxSupply    int `json:"maxSupply"`
 }
